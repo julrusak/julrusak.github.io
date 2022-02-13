710f5973d500b798beafe9a9b3411e9bd29e7164
Julianna Rusakiewicz <juliannarusak@gmail.com>
2022-02-12 19:06:14 -0500

    Delete script.js

--- js/script.js
+++
@@ -1,9 +0,0 @@
-$( document ).ready(function() {
-    $('#hamburger').on('click', function(e) {
-      $(this).toggleClass("open");  
-      $('nav').toggleClass("open");  
-      e.preventDefault();
-    });
-
-
-});
